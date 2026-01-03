a35f8f7ef3853e2320c0175f8593065f6ee1a526
Bokl2002 <abdullah.bokl@gmail.com>
2026-01-03 19:36:05 +0200

    feat(wallet): add wallet cubit and state management

---
+++ lib/features/wallet/presentation/bloc/wallet_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:boklo/core/base/base_cubit.dart';
+import 'package:boklo/core/base/base_state.dart';
+import 'package:boklo/features/wallet/domain/usecases/get_transactions_usecase.dart';
+import 'package:boklo/features/wallet/domain/usecases/get_wallet_usecase.dart';
+import 'package:boklo/features/wallet/presentation/bloc/wallet_state.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class WalletCubit extends BaseCubit<WalletState> {
+  final GetWalletUseCase _getWalletUseCase;
+  final GetTransactionsUseCase _getTransactionsUseCase;
+
+  WalletCubit(
+    this._getWalletUseCase,
+    this._getTransactionsUseCase,
+  ) : super(const BaseState.initial());
+
+  Future<void> loadWallet() async {
+    emitLoading();
+
+    final walletResult = await _getWalletUseCase();
+
+    walletResult.fold(
+      (error) => emitError(error),
+      (wallet) async {
+        final transactionsResult = await _getTransactionsUseCase();
+
+        transactionsResult.fold(
+          (error) => emitError(error),
+          (transactions) {
+            emitSuccess(
+              WalletState(
+                wallet: wallet,
+                transactions: transactions,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/features/wallet/presentation/bloc/wallet_state.dart
@@ -0,0 +1,13 @@
+import 'package:boklo/features/wallet/domain/entities/transaction_entity.dart';
+import 'package:boklo/features/wallet/domain/entities/wallet_entity.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'wallet_state.freezed.dart';
+
+@freezed
+class WalletState with _$WalletState {
+  const factory WalletState({
+    required WalletEntity wallet,
+    @Default([]) List<TransactionEntity> transactions,
+  }) = _WalletState;
+}
