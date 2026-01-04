42279962d6163e49d21395509336debaf450086f
Bokl2002 <abdullah.bokl@gmail.com>
2026-01-05 00:18:39 +0200

    feat(transfers): add transfer cubit and state management

---
+++ lib/features/transfers/presentation/bloc/transfer_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:boklo/core/base/base_cubit.dart';
+import 'package:boklo/core/base/base_state.dart';
+import 'package:boklo/features/transfers/domain/entities/transfer_entity.dart';
+import 'package:boklo/features/transfers/domain/usecases/create_transfer_usecase.dart';
+import 'package:boklo/features/transfers/presentation/bloc/transfer_state.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class TransferCubit extends BaseCubit<TransferState> {
+  TransferCubit(this._createTransferUseCase) : super(const BaseState.initial());
+
+  final CreateTransferUseCase _createTransferUseCase;
+
+  Future<void> createTransfer(TransferEntity transfer) async {
+    emitLoading();
+
+    final result = await _createTransferUseCase(transfer);
+
+    result.fold(
+      emitError,
+      (_) => emitSuccess(const TransferState()),
+    );
+  }
+}
---
+++ lib/features/transfers/presentation/bloc/transfer_state.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class TransferState extends Equatable {
+  const TransferState();
+
+  @override
+  List<Object?> get props => [];
+}
